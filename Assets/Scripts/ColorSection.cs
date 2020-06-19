e7139a7093b16822161f686f07b3cf89da6d0e11
Antonio <antonihipona@gmail.com>
2020-06-19 20:03:49 +0200

    Add material color change in customization screen

---
+++ Assets/Scripts/ColorSection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSection : MonoBehaviour
+{
+    private CustomizableColor _currentSelectedColor;
+
+    public void ChangeSelected(CustomizableColor _color)
+    {
+        if (_currentSelectedColor != null)
+            _currentSelectedColor.SetSelected(false);
+        _color.SetSelected(true);
+        _currentSelectedColor = _color;
+    }
+}
---
+++ Assets/Scripts/CustomizableColor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomizableColor : MonoBehaviour
+{
+    public Color color;
+    public GameObject tick;
+    public GameObject target;
+
+    private Button _button;
+    private ColorSection _colorSection;
+
+    private void Awake()
+    {
+        SetSelected(false);
+        _button = GetComponent<Button>();
+        GetComponent<Image>().color = color;
+        _colorSection = GetComponentInParent<ColorSection>();
+    }
+
+    private void Start()
+    {
+        _button.onClick.AddListener(() => _colorSection.ChangeSelected(this));
+    }
+
+    public void SetSelected(bool _isSelected)
+    {
+        if (_isSelected)
+        {
+            tick.SetActive(true);
+            var targetRenderers = target.GetComponentsInChildren<Renderer>();
+            foreach (var renderer in targetRenderers)
+            {
+                foreach (var material in renderer.materials)
+                {
+                    material.color = color;
+                }
+            }
+        }
+        else
+        {
+            tick.SetActive(false);
+        }
+
+    }
+}
